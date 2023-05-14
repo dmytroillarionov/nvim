290a34531386523f1ac493b5d30cf95527051d2b
Dimitri Illarionov <dimitri.illarionov@gmail.com>
2023-05-14 15:15:46 +0000

    [WIP] nvim config

--- nvim/lua/setup/plugins-setup.lua
+++ nvim/lua/setup/plugins-setup.lua
@@ -31,18 +31,32 @@ return packer.startup(function(use)
   -- packer can manage itself
   use("wbthomason/packer.nvim")
 
+  use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+
   use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
 
   use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
   use("szw/vim-maximizer") -- maximizes and restores current window
 
-    -- commenting with gc
-  use("numToStr/Comment.nvim")
-
   -- essential plugins
   use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
   use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+  
+    -- file explorer
+  use("nvim-tree/nvim-tree.lua")
+
+    -- vs-code like icons
+  use("nvim-tree/nvim-web-devicons")
+
+  -- statusline
+  use("nvim-lualine/lualine.nvim")
+
+
+   -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
 
 if packer_bootstrap then
     require("packer").sync()
